00bf8bb787f8f64477d9b421b2fb5ec3f723bab9
yodoj <nadiraalia4@gmail.com>
2024-09-24 05:34:42 +0700

    Membuat autentikasi, session, dan cookies

--- dollova_store/settings.py
+++ dollova_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b_lzb11!%ga#tc^&am#%je@z!o1m6yxf_va26)bgcmb8(p9p62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nadira-aliya-dollovastore.pbp.cs.ui.ac.id"]
 
--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
